a2e7f641adce57f926d4b5f219401cadefb4abc1
Pablo GoÃÅmez <pgomez@redsauce.net>
2016-07-19 00:09:56 +0200

    First part of challenge "Velocimetry", including the enum and part of the class

--- velocimetro.playground/Contents.swift
+++ velocimetro.playground/Contents.swift
@@ -2,4 +2,26 @@
 
 import UIKit
 
-var str = "Hello, playground"
+enum Velocidades: Int{
+    
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    
+    init( velocidadInicial : Velocidades ){
+        self = velocidadInicial
+    }
+    
+}
+
+class Auto {
+    var velocidad = Velocidades(rawValue: 0)
+    
+    init( velocidad : Velocidades ){
+        self.velocidad = velocidad
+    }
+    
+    func cambioDeVelocidad( ) -> ( actual: Int, velocidadEnCadena: String){
+        
+    }
+    
+    
+}
