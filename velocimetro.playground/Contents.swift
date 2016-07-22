51283b14b100012d912b8c25cb0b547aa9fc79dd
Pablo GoÃÅmez <pgomez@redsauce.net>
2016-07-23 01:36:43 +0200

    Commit with the last version of velocimetro playground, challenge week 4

--- velocimetro.playground/Contents.swift
+++ velocimetro.playground/Contents.swift
@@ -3,17 +3,18 @@
 import UIKit
 
 enum Velocidades: Int{
-    
+
     case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
     
-    init( velocidadInicial : Velocidades ){
-        self = velocidadInicial
+    init( velocidadInicial: Velocidades ){
+        self = .Apagado
     }
     
 }
 
 class Auto {
-    var velocidad = Velocidades(rawValue: 0)
+    
+    var velocidad = Velocidades(velocidadInicial: Velocidades.Apagado)
     
     init( velocidad : Velocidades ){
         self.velocidad = velocidad
@@ -21,7 +22,28 @@ class Auto {
     
     func cambioDeVelocidad( ) -> ( actual: Int, velocidadEnCadena: String){
         
-    }
-    
-    
+        if velocidad.rawValue == 0 {
+            velocidad = Velocidades.VelocidadBaja
+            
+        } else if velocidad.rawValue == 20 {
+            velocidad = Velocidades.VelocidadMedia
+            
+        } else if velocidad.rawValue == 50 {
+            velocidad = Velocidades.VelocidadAlta
+            
+        } else {
+            velocidad = Velocidades.VelocidadMedia }
+        
+         return (velocidad.rawValue, "\(velocidad)")
+       
+      }
+}
+
+
+var miAuto = Auto.init(velocidad: Velocidades.Apagado)
+
+for index in 1...20 {
+    if index == 1 {print ( "\(miAuto.velocidad.rawValue), \(miAuto.velocidad)" )}
+    miAuto.cambioDeVelocidad()
+    print ( "\(miAuto.velocidad.rawValue), \(miAuto.velocidad)" )
 }
